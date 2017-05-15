3e6f3c9e9db3cae90bdfa31da7d984ffeb1f57a2
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-05-15 16:46:41 +0100

    TP6 - exercicio 4
    Corpo do submarino corrigido:
         - direcções
         - dimensões

--- TP6/tp6/MySubmarine.js
+++ TP6/tp6/MySubmarine.js
@@ -10,6 +10,7 @@ function MySubmarine(scene) {
 	this.cylinder = new MyCylinder(scene,12,1);
 	this.semiEsfera1 = new MyLamp(scene,12,3);
 	this.semiEsfera2 = new MyLamp(scene,12,3);
+	this.cylinderTop = new MyCylinder(scene,12,1);
 
 	this.initBuffers();
 };
@@ -21,23 +22,22 @@ MySubmarine.prototype.display = function(){
 
 	//corpo
 	this.scene.pushMatrix();
-		this.scene.scale(0.73, 1, 4.08);
+		this.scene.scale(4.08, 1, 0.73);
+		this.scene.rotate(Math.PI/2, 0, 1, 0);
 		this.cylinder.display();
 	this.scene.popMatrix();
 
+	//semiEsfera1 =  frente
 	this.scene.pushMatrix();
-		this.scene.scale(0.73, 1, 4.08);
-		this.cylinder.display();
-	this.scene.popMatrix();
-
-	this.scene.pushMatrix();
-		this.scene.rotate(-(Math.PI), 1, 0, 0);
+		this.scene.translate(4.08, 0, 0);
+		this.scene.rotate((Math.PI)/2, 0, 1, 0);
 		this.scene.scale(0.73, 1, 1);
 		this.semiEsfera1.display();
 	this.scene.popMatrix();
 
+	//semiEsfera2 =  trás
 	this.scene.pushMatrix();
-		this.scene.translate(0, 0, 4.08);
+		this.scene.rotate(-(Math.PI)/2, 0, 1, 0);
 		this.scene.scale(0.73, 1, 1);
 		this.semiEsfera1.display();
 	this.scene.popMatrix();
